b1d341e5e9f99358204097358f359a8627487252
j4n-r <jan.ruegge@gmail.com>
2025-05-08 21:14:52 +0200

    change emacs shell to flake

--- shells/emacs-libvterm.nix
+++ shells/emacs-libvterm.nix
@@ -1,12 +1,29 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  description = "Flake to build emacs vterm";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
 
-pkgs.mkShell {
-  name = "emacsenv";
-  buildInputs = [
-    pkgs.cmake
-    pkgs.enchant
-    pkgs.pkg-config
-    pkgs.libvterm-neovim
-    pkgs.libvterm
-  ];
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacypackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            enchant
+            pkg-config
+            libvterm-neovim # test next time which one works
+            libvterm
+          ];
+        };
+      }
+    );
 }
