e82d315dd33be5fd276aa7537812b70f2828d1bc
Jan Rüggeberg <jan.rueggeberg@mirantus.com>
2025-05-20 16:00:15 +0200

    clean up mac settings

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -12,26 +12,13 @@
 ###################################################################################
 {
   system = {
+    primaryUser = "JR";
     stateVersion = 5;
-    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    activationScripts.postUserActivation.text = ''
-      # activateSettings -u will reload the settings from the database and apply them to the current session,
-      # so we do not need to logout and login again to make the changes take effect.
-      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-    '';
-
     defaults = {
-      menuExtraClock.Show24Hour = true; # show 24 hour clock
-      # customize dock
+      menuExtraClock.Show24Hour = true;
       dock = {
         autohide = true;
-        show-recents = false; # disable recent apps
-
-        # customize Hot Corners(触发角, 鼠标移动到屏幕角落时触发的动作)
-        # wvous-tl-corner = 2; # top-left - Mission Control
-        # wvous-tr-corner = 13; # top-right - Lock Screen
-        # wvous-bl-corner = 3; # bottom-left - Application Windows
-        # wvous-br-corner = 4; # bottom-right - Desktop
+        show-recents = false;
       };
 
       # customize finder
@@ -46,8 +33,7 @@
 
       # customize trackpad
       trackpad = {
-        # tap - 轻触触摸板, click - 点击触摸板
-        Clicking = true; # enable tap to click(轻触触摸板相当于点击)
+        Clicking = true; # enable tap to click
         TrackpadRightClick = true; # enable two finger right click
         TrackpadThreeFingerDrag = true; # enable three finger drag
       };
@@ -63,13 +49,13 @@
         AppleKeyboardUIMode = 3; # Mode 3 enables full keyboard control.
         ApplePressAndHoldEnabled = true; # enable press and hold
 
-        # NSAutomaticCapitalizationEnabled = false; # disable auto capitalization(自动大写)
-        # NSAutomaticDashSubstitutionEnabled = false; # disable auto dash substitution(智能破折号替换)
-        # NSAutomaticPeriodSubstitutionEnabled = false; # disable auto period substitution(智能句号替换)
-        # NSAutomaticQuoteSubstitutionEnabled = false; # disable auto quote substitution(智能引号替换)
-        # NSAutomaticSpellingCorrectionEnabled = false; # disable auto spelling correction(自动拼写检查)
-        # NSNavPanelExpandedStateForSaveMode = true; # expand save panel by default(保存文件时的路径选择/文件名输入页)
-        # NSNavPanelExpandedStateForSaveMode2 = true;
+        NSAutomaticCapitalizationEnabled = false; # disable auto capitalization
+        NSAutomaticDashSubstitutionEnabled = false; # disable auto dash substitution
+        NSAutomaticPeriodSubstitutionEnabled = false; # disable auto period substitution
+        NSAutomaticQuoteSubstitutionEnabled = false; # disable auto quote substitution
+        NSAutomaticSpellingCorrectionEnabled = false; # disable auto spelling correction
+        NSNavPanelExpandedStateForSaveMode = true; # expand save panel by default
+        NSNavPanelExpandedStateForSaveMode2 = true;
       };
 
       # Customize settings that not supported by nix-darwin directly
@@ -137,17 +123,6 @@
     # the most important thing is to remap option key to alt key globally,
     # but it's not supported by macOS yet.
     keyboard = {
-      enableKeyMapping = true; # enable key mapping so that we can use `option` as `control`
-
-      # NOTE: do NOT support remap capslock to both control and escape at the same time
-      remapCapsLockToControl = false; # remap caps lock to control, useful for emac users
-      remapCapsLockToEscape = false; # remap caps lock to escape, useful for vim users
-
-      # swap left command and left alt
-      # so it matches common keyboard layout: `ctrl | command | alt`
-      #
-      # disabled, caused only problems!
-      # swapLeftCommandAndLeftAlt = false;
       swapLeftCtrlAndFn = true;
     };
   };
